9a04b90a40e85414cc2d01521bcf387d9aefa261
bala-amypo <bala@amypo.in>
2025-12-17 10:27:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/RewardRule.java
+++ src/main/java/com/example/demo/entity/RewardRule.java
@@ -14,14 +14,17 @@ public class RewardRule{
     public Long getId(){
         return id;
     }
-    public void setId(){
+    public void setId(Long id){
         this.id=id;
     }
      public Long getCardId(){
         return cardId;
     }
-    public void setCard(){
-        this.id=id;
+    public void setCardId(Long cardId){
+        this.cardId=cardId;
     }
-
+    public String getCategory(){
+        return category;
+    }
+    public void setC
 }
--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -20,49 +20,49 @@ public class UserProfile{
     public Long getId(){
         return id;
     }
-    public void setId(){
+    public void setId(Long id){
         this.id=id;
     }
     public String getUserId(){
         return userId;
     }
-    public void setUserId(){
+    public void setUserId(String userId){
         this.userId=userId;
     }
      public String getFullName(){
         return fullName;
     }
-    public void setFullName(){
+    public void setFullName(String fullName){
         this.fullName=fullName;
     }
      public String getEmail(){
         return email;
     }
-    public void setEmail(){
+    public void setEmail(String email){
         this.email=email;
     }
      public String getPassword(){
         return password;
     }
-    public void setPassword(){
+    public void setPassword(String password){
         this.password=password;
     }
      public String getRole(){
         return role;
     }
-    public void setRole(){
+    public void setRole(String role){
         this.role=role;
     }
      public Boolean getActive(){
         return active;
     }
-    public void setActive(){
+    public void setActive(Boolean active){
         this.active=active;
     }
     public LocalDateTime getCreatedAt(){
         return createdAt;
     }
-    public void setCreatedAt(){
+    public void setCreatedAt(LocalDateTime createdAt){
         this.createdAt=createdAt;
     }
 
