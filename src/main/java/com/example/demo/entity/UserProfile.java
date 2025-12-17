6afbfe9efbf8dafef6817e2283fc1d34e6d9a285
bala-amypo <bala@amypo.in>
2025-12-17 09:02:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -1,12 +1,12 @@
-// package com.example.demo.entity;
-// import jakarta.perisistance.Entity;
-// @Entity
-// public class UserProfile{
-//     @Id
-//     private Long id;
-//     @column(unique=true)
-//     private String userId;
-//     private String fullName;
-//     @column(unique=true)
-//     private String email;
-// }
+package com.example.demo.entity;
+import jakarta.perisistance.Entity;
+@Entity
+public class UserProfile{
+    @Id
+    private Long id;
+    @column(unique=true)
+    private String userId;
+    private String fullName;
+    @column(unique=true)
+    private String email;
+}
