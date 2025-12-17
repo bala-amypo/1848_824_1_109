a2e43edfa5c5bd2333b34c0040408b89869bfde7
bala-amypo <bala@amypo.in>
2025-12-17 10:22:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/RewardRule.java
+++ src/main/java/com/example/demo/entity/RewardRule.java
@@ -2,4 +2,26 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 @Entity
-public 
+public class RewardRule{
+    @Id
+    private Long id;
+    private Long cardId;
+    private String category;
+    private String rewardType;
+    private Double multiplier;
+    private Boolean active;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+    }
+     public Long getCardId(){
+        return cardId;
+    }
+    public void setCard(){
+        this.id=id;
+    }
+
+}
