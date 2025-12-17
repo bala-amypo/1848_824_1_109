b0a9a42fe4ee57faba892714798eb9e69c57a666
bala-amypo <bala@amypo.in>
2025-12-17 06:24:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/CreditCardRecord.java
+++ src/main/java/com/example/demo/entity/CreditCardRecord.java
@@ -2,8 +2,8 @@ package com.example.demo.entity;
 import.jakarta.perisistance.Entity;
 @Entity
 public class CreditCardRecord{
-    private long id;
-    private long userId;
+    private Long id;
+    private Long userId;
     private String cardName;
     private String issuer;
     private String cardType;
@@ -16,6 +16,15 @@ public class CreditCardRecord{
     }
     public void setId(Long id){
         this.id=id;
+    }
+    public Long getUserId(){
+        return userId;
+    }
+    public void setUserId(Long userId){
+        this.userId=userId;
+    }
+    public String getCardName(){
+        return cardName;
     }
     
 }
