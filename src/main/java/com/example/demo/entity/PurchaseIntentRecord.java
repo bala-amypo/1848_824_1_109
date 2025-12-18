82373938d5b50f6c1f61b6309600b9faa1f9c5bf
bala-amypo <bala@amypo.in>
2025-12-18 05:00:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/PurchaseIntentRecord.java
+++ src/main/java/com/example/demo/entity/PurchaseIntentRecord.java
@@ -18,5 +18,29 @@ public class PurchaseIntentRecord{
     public void setId(Long id){
         this.id=id;
     }
-    public Long getUserId()
+    public Long getUserId(){
+        return userId;
+    }
+    public void setUserId(Long userId){
+        this.userId=userId;
+    }
+    public Double getAmount(){
+        return amount;
+    }
+    public void setAmount(Double amount){
+        this.amount=amount;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public void setCategory(String category){
+        this.category=category;
+    }
+    public String getMerchant(){
+        return merchant;
+    }
+    public void setMerchant(String merchant){
+        this.merchant=merchant;
+    }
+    public LocalDateTime 
 }
