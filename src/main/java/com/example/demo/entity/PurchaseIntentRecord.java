1988ab5ab5eea5212769d229af616c77fe02eba4
bala-amypo <bala@amypo.in>
2025-12-18 03:55:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/PurchaseIntentRecord.java
+++ src/main/java/com/example/demo/entity/PurchaseIntentRecord.java
@@ -1,2 +1,13 @@
 package com.example.demo.entity;
-import jakarta.persistence.Entity
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class PurchaseIntentRecord{
+    @Id
+    private Long id;
+    private Long userId;
+    private Double amount;
+    private String category;
+    private String merchant;
+    
+}
