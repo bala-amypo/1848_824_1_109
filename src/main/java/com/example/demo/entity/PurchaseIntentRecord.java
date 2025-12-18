9d1c122babca2068ebbfa0c0fb9642b63f087c0e
bala-amypo <bala@amypo.in>
2025-12-18 04:50:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/PurchaseIntentRecord.java
+++ src/main/java/com/example/demo/entity/PurchaseIntentRecord.java
@@ -4,10 +4,11 @@ import jakarta.persistence.Id;
 @Entity
 public class PurchaseIntentRecord{
     @Id
+    @GeneratedValue(strategy=GeneratedType.IDENTITY)
     private Long id;
     private Long userId;
     private Double amount;
     private String category;
     private String merchant;
-    
+
 }
