efaf2cb7caea3274d4c4d614a7d5b7c604d7ce91
bala-amypo <bala@amypo.in>
2025-12-18 09:40:30 +0000

    User commit

--- src/main/java/com/example/demo/service/UserProfileService.java
+++ src/main/java/com/example/demo/service/UserProfileService.java
@@ -1,10 +1,10 @@
 package com.example.demo.service;
-
+import com.example.demo.entity.UserProfile;
 
 
 public interface UserProfileService
 {
     UserProfile createUser(UserProfile profile);
-    Long getUserById(Long id);
+    
 
 }
--- src/main/java/com/example/demo/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserProfileServiceImpl.java
@@ -3,7 +3,8 @@ import com.example.demo.repository.UserProfileRepository;
 import com.example.demo.entity.UserProfile;
 import com.example.demo.service.UserProfileService;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Service
 public class UserProfileServiceImpl implements UserProfileService{
