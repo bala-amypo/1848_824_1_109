2cdf0572612d18a6963337a9da40509f512f88bb
bala-amypo <bala@amypo.in>
2025-12-19 08:31:07 +0000

    User commit

--- src/main/java/com/example/demo/service/UserProfileService.java
+++ src/main/java/com/example/demo/service/UserProfileService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 import com.example.demo.entity.UserProfile;
 
 
-interface UserProfileService
+public interface UserProfileService
 {
     UserProfile createUserProfile(UserProfile profile);
     UserProfile getUserById(Long id);
