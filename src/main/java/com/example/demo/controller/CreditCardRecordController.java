3d9e22fda73309e3c9aa781f6d95e44799dc2689
bala-amypo <bala@amypo.in>
2025-12-20 18:25:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/CreditCardRecordController.java
+++ src/main/java/com/example/demo/controller/CreditCardRecordController.java
@@ -13,55 +13,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-package com.example.demo.controller;
-
-import java.util.List;
-
-import org.springframework.web.bind.annotation.*;
-
-import com.example.demo.entity.CreditCardRecord;
-import com.example.demo.service.CreditCardService;
-
 @RestController
 @RequestMapping("/api/cards")
 public class CreditCardController {
 
-    private final CreditCardService creditCardService;
-
-    public CreditCardController(CreditCardService creditCardService) {
-        this.creditCardService = creditCardService;
-    }
-
-    // Add new card
+    CreditCardService ccs;
     @PostMapping
     public CreditCardRecord addCard(@RequestBody CreditCardRecord card) {
-        return creditCardService.addCard(card);
+        return ccs.addCard(card);
     }
-
-    // Update card
     @PutMapping("/{id}")
     public CreditCardRecord updateCard(
             @PathVariable Long id,
             @RequestBody CreditCardRecord updated) {
 
-        return creditCardService.updateCard(id, updated);
+        return ccs.updateCard(id, updated);
     }
-
-    // Get card by ID
     @GetMapping("/{id}")
     public CreditCardRecord getCardById(@PathVariable Long id) {
-        return creditCardService.getCardById(id);
+        return ccs.getCardById(id);
     }
-
-    // Get cards by user ID
     @GetMapping("/user/{userId}")
     public List<CreditCardRecord> getCardsByUser(@PathVariable Long userId) {
-        return creditCardService.getCardsByUser(userId);
+        return ccs.getCardsByUser(userId);
     }
-
-    // Get all cards
     @GetMapping
     public List<CreditCardRecord> getAllCards() {
-        return creditCardService.getAllCards();
+        return ccs.getAllCards();
     }
 }
