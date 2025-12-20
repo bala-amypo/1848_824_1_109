6d5814de6121a777b905f9766f8c244b44217576
bala-amypo <bala@amypo.in>
2025-12-20 17:34:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserProfileServiceImpl.java
@@ -18,7 +18,7 @@ return upr.save(profile);
 }
 @Override
 public UserProfile getUserById(Long id){
-    return upr.findById(id);
+    return upr.findUserById(id);
 }
 @Override
 public UserProfile findByUserId(String userId){
