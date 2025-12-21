b1d70d88b0b2759a4e08559bf5230122b60747d2
bala-amypo <bala@amypo.in>
2025-12-21 06:32:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/CreditCardRecord.java
+++ src/main/java/com/example/demo/entity/CreditCardRecord.java
@@ -45,6 +45,9 @@ public class CreditCardRecord {
     public Long getId() {
         return id;
     }
+    public void setId(Long id){
+        this.id=id;
+        }
 
     public Long getUserId() {
         return userId;
