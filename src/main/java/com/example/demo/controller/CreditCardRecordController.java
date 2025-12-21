0b5b1dbc18e59cf2fe03129903c8173ac1c1812e
bala-amypo <bala@amypo.in>
2025-12-21 06:42:33 +0000

    User commit

--- src/main/java/com/example/demo/controller/CreditCardRecordController.java
+++ src/main/java/com/example/demo/controller/CreditCardRecordController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RestController
