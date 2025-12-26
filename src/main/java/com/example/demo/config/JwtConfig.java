21b33cedbdada49840b720d3c53dbde0d64992f0
bala-amypo <bala@amypo.in>
2025-12-26 05:10:46 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/JwtConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo.config;
+
+import com.example.demo.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public JwtUtil jwtUtil(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expiration) {
+
+        return new JwtUtil(secret.getBytes(), expiration);
+    }
+}
